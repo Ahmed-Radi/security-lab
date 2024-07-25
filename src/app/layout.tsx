fdac247c2fceb55d28158ce8e1a9da7e12e2adfb
ahmed-radi <ahmedradi743@gmail.com>
2024-07-25 19:41:50 +0300

    feat(ui): create Navbar component
    - Added Navbar component to the layout
    - Integrated Navbar into the RootLayout component
    - Applied global styles and font settings

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Plus_Jakarta_Sans } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
+import Navbar from "@/components/navbar";
 
 const fontSans = Plus_Jakarta_Sans({
   subsets: ["latin"],
@@ -27,7 +28,10 @@ export default function RootLayout({
           fontSans.variable
         )}
       >
-        {children}
+        <Navbar />
+        <div className="pt-[66px] h-screen w-full">
+          {children}
+        </div>
       </body>
     </html>
   );
